e05328ea7b7089be0d7f05822ca9e0f580f779f8
Chirag Toprani <chiragT.007@gmail.com>
2016-02-11 17:27:39 -0800

    main page working well

--- dist/js/navbar.js
+++ dist/js/navbar.js
@@ -5,6 +5,7 @@ jQuery(document).ready(function($) {
     $(window).scrollTop(0);
 });
   //  space.height(originalheight + 'em');
+
     var navbarmain = $('#navbar-main'),
         distance = navbarmain.offset().top,
         $window = $(window);
@@ -15,7 +16,10 @@ jQuery(document).ready(function($) {
 
     distancebetween = navoffset - distance - navbarmain.height();
 
+    var paddT = $('.content').css('padding-top');
+    console.log(paddT);
 
+    var padding = false;
     $window.scroll(function() {
         if ($window.scrollTop() > distance) {
           //  $(".right").removeClass("rightundo");
@@ -33,39 +37,70 @@ jQuery(document).ready(function($) {
             $("#main").removeClass('after');     */
         }
 
-        console.log(distancebetween);
         if ($window.scrollTop() < distancebetween){
+          $('section').removeClass('removeShadow')
+
           navbarmain.addClass('fixed');
           navbarmain.removeClass('absolute');
-
+          $('.content').css("padding-top", paddT);
+          $('#space').height(0);
         } else if ( $window.scrollTop() > distancebetween){
+          //reached matching point
+          $('section').addClass('removeShadow')
           navbarmain.removeClass('fixed');
           navbarmain.addClass('absolute');
-          console.log("fix top");
+          $('#space').height(0);
+
         }
 
-        if ($window.scrollTop() >= navoffset - navbarmain.height()){
+
+        if ($window.scrollTop() >= navoffset - navbarmain.height()  ){
             navbarmain.addClass('fixednav');
-            var newtop =navbarmain.height();
+            var newtop =navbarmain.height();;
+
+            navbarmain.removeClass('absolute');
+
+            $('.key').addClass('finalnav');
+
+            navbarmain.addClass('fixednav');
             navigation.addClass('fixednav');
             navigation.css({top:newtop});
 
+            if ($window.scrollTop() <= navoffset ){
+              var height = $window.scrollTop() -  $('#space').offset().top +navbarmain.height() + navigation.height();
+              $('#space').height(height);
+            }
 
-            //animations
-            $(".name").addClass('after');
-            $(".right").removeClass("rightundo");
-            $(".left").removeClass("leftundo");
-            $(".label").addClass('after');
         } else{
             navbarmain.removeClass('fixednav');
             navigation.removeClass('fixednav');
             navigation.css({top:''});
 
+          }
+
+          if ($window.scrollTop() >= navoffset){
+            //var newpadding = $window.scrollTop() - (navoffset - navbarmain.height() - navigation.height()) + paddT;
+            //$('.content').css("padding-top", newpadding);
             //animations
+             $(".name").addClass('after');
+             $(".right").removeClass("rightundo");
+             $(".left").removeClass("leftundo");
+             $(".label").addClass('after');
+             $(".navbar").addClass('next');
+             $(".lower").addClass('move');
+             $(".lower").addClass('movelower');
+             $(".upper").addClass('moveupper');
+             $(".display").addClass('after');
+          }else {
+           //animations
             $(".name").removeClass('after');
             $(".label").removeClass('after');
              $(".navlist").removeClass('after');
              $("#main").removeClass('after');
+             $(".navbar").removeClass('next');
+             $(".lower").removeClass('movelower');
+             $(".upper").removeClass('moveupper');
+             $(".display").removeClass('after');
         }
 
 
